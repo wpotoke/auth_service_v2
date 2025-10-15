a338ef4e377d57607e898b2a4e0d6ae9e73b2cc6
moroz <morozovdaniel713@gmail.com>
2025-10-15 22:12:32 +0300

    feat(models): implemet user model with minimum necessary field

---
+++ app/models/users.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Boolean, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+from app.core.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, unique=True, nullable=False
+    )
+    email: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
