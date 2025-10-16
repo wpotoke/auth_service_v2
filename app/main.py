67f6efe68fee63e37cb604c57874329cb89ee406
moroz <morozovdaniel713@gmail.com>
2025-10-16 18:18:23 +0300

    add main core of proj, setup cors and include user router

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.routers.users import router as user_router
+
+app = FastAPI(
+    title="FastAPI ecommerce - Интеренет магазин",
+    version="0.1.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(user_router)
+
+
+@app.get("/")
+async def root():
+    """Корневой маршрут, подверждающий, что API работает."""
+    return {"message": "Добро пожаловать в user service"}
