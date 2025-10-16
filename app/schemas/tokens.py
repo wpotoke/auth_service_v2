e3e9dacb8a44e2b8769f56feac13874493a728fd
moroz <morozovdaniel713@gmail.com>
2025-10-16 18:00:31 +0300

    refactot(schemas): edit length token from 40 to 200 symbols

--- app/schemas/tokens.py
+++ app/schemas/tokens.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field
 
 
 class RefreshTokenBase(BaseModel):
-    token: str = Field(min_length=145, max_length=200)
+    token: str = Field(min_length=40, max_length=200)
     user_id: int
     expires_at: datetime
 
