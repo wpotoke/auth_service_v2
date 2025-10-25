40097fc313e9d6522545c1c0dbf17c981866187d
moroz <morozovdaniel713@gmail.com>
2025-10-26 00:31:31 +0300

    feat(cache): add cache lazy loading in services

--- task_service/app/api/routers/tasks.py
+++ task_service/app/api/routers/tasks.py
@@ -1,6 +1,11 @@
 from typing import Annotated, Optional
+import redis.asyncio as redis
 from fastapi import APIRouter, Depends, Path, status
-from task_service.app.core.dependencies import get_task_service, get_current_user
+from task_service.app.core.dependencies import (
+    get_task_service,
+    get_current_user,
+    get_redis_client,
+)
 from task_service.app.services.tasks import TaskService
 from task_service.app.schemas.tasks import Task, TaskCreate
 
@@ -15,22 +20,28 @@ async def get_tasks(
     return await task_service.get_tasks(user_id=user_id)
 
 
-@router.get("/{task_id}", response_model=list[Task], status_code=status.HTTP_200_OK)
+@router.get("/{task_id}", response_model=Task, status_code=status.HTTP_200_OK)
 async def get_task(
     task_id: Annotated[int, Path(ge=1)],
     task_service: Annotated[TaskService, Depends(get_task_service)],
     user_id: Annotated[int, Depends(get_current_user)],
+    redis_client: Annotated[redis.Redis, Depends(get_redis_client)],
 ) -> Optional[Task]:
-    return await task_service.get_task_by_id(task_id=task_id, user_id=user_id)
+    return await task_service.get_task_by_id(
+        task_id=task_id, user_id=user_id, r=redis_client
+    )
 
 
-@router.post("", response_model=Optional[Task], status_code=status.HTTP_201_CREATED)
+@router.post("", response_model=Task, status_code=status.HTTP_201_CREATED)
 async def create_task(
     task_service: Annotated[TaskService, Depends(get_task_service)],
     user_id: Annotated[int, Depends(get_current_user)],
     task_create: TaskCreate,
+    redis_client: Annotated[redis.Redis, Depends(get_redis_client)],
 ) -> Optional[Task]:
-    return await task_service.create_task(task_create=task_create, user_id=user_id)
+    return await task_service.create_task(
+        task_create=task_create, user_id=user_id, r=redis_client
+    )
 
 
 @router.put("/{task_id}", response_model=Task, status_code=status.HTTP_200_OK)
--- task_service/app/services/tasks.py
+++ task_service/app/services/tasks.py
@@ -1,35 +1,50 @@
 from typing import Optional
+import json
+import redis.asyncio as redis
 from task_service.app.repositories.tasks import TaskRepository
 from task_service.app.models.task import Task as TaskModel
-from task_service.app.schemas.tasks import TaskCreate
+from task_service.app.schemas.tasks import TaskCreate, Task as TaskSchema
 
 
 class TaskService:
     def __init__(self, task_repository: TaskRepository):
         self.task_repository = task_repository
 
-    async def get_tasks(self, user_id) -> list[TaskModel]:
+    async def get_tasks(self, user_id: int) -> list[TaskModel]:
         return await self.task_repository.get_all(user_id=user_id)
 
-    async def get_task_by_id(self, task_id: int, user_id) -> Optional[TaskModel]:
-        task = await self.task_repository.get_by_id(task_id=task_id, user_id=user_id)
-        if not task:
+    async def get_task_by_id(
+        self, task_id: int, user_id: int, r: redis.Redis
+    ) -> Optional[TaskModel | TaskSchema]:
+        cache_key = f"task:{task_id}-user:{user_id}"
+        cached = await r.get(cache_key)
+        if cached:
+            data = json.loads(cached)
+            return TaskSchema.model_validate_json(data)
+        task_model = await self.task_repository.get_by_id(
+            task_id=task_id, user_id=user_id
+        )
+        if not task_model:
             raise ValueError("...")
-        return task
+        task_schema = TaskSchema.model_validate(task_model)
+        task_json = json.dumps(task_schema.model_dump_json())
+        await r.setex(name=cache_key, value=task_json, time=60)
+        return task_model
 
     async def create_task(
-        self, task_create: TaskCreate, user_id
+        self, task_create: TaskCreate, user_id: int, r: redis.Redis
     ) -> Optional[TaskModel]:
         if task_create.user_id != user_id:
-            raise ValueError(f"{user_id} not qe {task_create.user_id}")
+            raise ValueError(f"{user_id} not qe {user_id}")
         task = await self.task_repository.create(task_create=task_create)
-        print()
         if not task:
             raise ValueError("...")
+        cache_key = f"task:{task.id}-user:{user_id}"
+        await r.delete(cache_key)
         return task
 
     async def task_update(
-        self, task_id: int, task_update: TaskCreate, user_id
+        self, task_id: int, task_update: TaskCreate, user_id: int
     ) -> Optional[TaskModel]:
         return await self.task_repository.update(
             task_id=task_id, task_update=task_update, user_id=user_id
